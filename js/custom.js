79dddaeae4d26414677963355ee5ff230378af28
ciceroduvall23 <74693314+ciceroduvall23@users.noreply.github.com>
2023-02-15 15:30:11 -0300

    subindo o repositorio

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // COUNTER NUMBERS
+    jQuery('.counter-thumb').appear(function() {
+      jQuery('.counter-number').countTo();
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('.hero-section').backstretch([
+      "images/plano de fundo-T.I.jpg", 
+      "images/plano de fundo-T.I2.jpg",
+      "images/plano de fundo3.jpg",
+     
+    ],  {duration: 2500, fade: 750});
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+  
+      scrollToDiv(elWrapped);
+      return false;
+  
+      function scrollToDiv(element){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+    
+  })(window.jQuery);
+
+
